fdffe0d2c738d0f8327f228544839c9ced6b0a3e
Garry_host <60875078+9xxxxxx@users.noreply.github.com>
2025-02-13 08:16:31 +0800

    Delete src directory

--- src/getdata/__init__.py
+++
@@ -1,5 +0,0 @@
-from .get_dc_data import data_wait4check_and_detect
-from .get_sendgoods_data import make_sendgoods_data
-from .get_sf_backlog_data import get_sf_data
-
-__all__ = ['data_wait4check_and_detect', 'make_sendgoods_data', 'get_sf_data']
